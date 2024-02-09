cb68eb214e85ef9b4c5a41e6db008921cd38508c
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2024-02-09 15:21:26 +0530

    Day 9

---
+++ Day 9/Day9.ts
@@ -0,0 +1,12 @@
+/*
+ * Answer : Option [d](All of the above)
+ *
+ * Explanation:
+ *
+ * a. Express: Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications. It simplifies the process of writing server code, and is one of the fundamental libraries for most Node.js applications.
+ *
+ * b. Body-parser: This is a middleware module for handling JSON, Raw, Text and URL encoded form data. It was a part of Express.js earlier but now you have to install it separately. This module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request.
+ *
+ * c. Socket.io: Socket.IO is a JavaScript library for real-time web applications. It enables real-time, bi-directional communication between web clients and servers. It has two parts: a client-side library that runs in the browser, and a server-side library for Node.js.
+ *
+ */
